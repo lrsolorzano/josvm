a8fc16b6a99e2f466f09e6459348f6ce75b68a5a
qiuyu <qiuyu@cs.unc.edu>
2017-03-10 14:14:18 -0500

    add panic to kernel page fault

--- kern/trap.c
+++ kern/trap.c
@@ -269,8 +269,11 @@ page_fault_handler(struct Trapframe *tf)
 	fault_va = rcr2();
 
 	// Handle kernel-mode page faults.
-
 	// LAB 3: Your code here.
+
+	if (tf->tf_cs == GD_KT)
+		panic("Page fault in kernel!\n");
+
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
