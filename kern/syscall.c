3f90d0f229247acdd8347822455d1dc6905e4ca6
qiuyu <qiuyu@cs.unc.edu>
2017-03-10 11:03:09 -0500

    Exercise 7 passed!

--- kern/syscall.c
+++ kern/syscall.c
@@ -75,10 +75,13 @@ syscall(uint64_t syscallno, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, 
 	// Return any appropriate return value.
 	// LAB 3: Your code here.
 
-	panic("syscall not implemented");
+//	panic("syscall not implemented");
 
 	switch (syscallno) {
-		
+	case SYS_cputs: sys_cputs((char*)a1, (size_t)a2); return 0;
+	case SYS_cgetc: return sys_cgetc();
+	case SYS_env_destroy: return sys_env_destroy((envid_t)a1);
+	case SYS_getenvid: return sys_getenvid();	
 	default:
 		return -E_NO_SYS;
 	}
